7b5fd682010025f11180d2fb1cf97df2e2e8344b
Ignacio-Licciardi <nacholicciardidev@gmail.com>
2023-11-07 10:59:08 -0300

    Agrego DTO Base

--- src/main/java/com/utn/sprint3/dtos/DtoBase.java
+++ src/main/java/com/utn/sprint3/dtos/DtoBase.java
@@ -1,2 +1,16 @@
-package com.utn.sprint3.dtos;public class DtoBase {
+package com.utn.sprint3.dtos;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class DtoBase implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/com/utn/sprint3/dtos/DtoMovimientosMonetarios.java
+++ src/main/java/com/utn/sprint3/dtos/DtoMovimientosMonetarios.java
@@ -1,5 +1,6 @@
 package com.utn.sprint3.dtos;
 
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,11 +8,13 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class DtoMovimientosMonetarios {
+
+
+public class DtoMovimientosMonetarios extends DtoBase {
 
     private BigDecimal ingresos;
     private BigDecimal costos;
